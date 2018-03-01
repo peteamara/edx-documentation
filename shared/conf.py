a13b8a0908ae0f2562451e40a37b8d6f764f6b29
Peter Amara <36055905+peteamara@users.noreply.github.com>
2018-03-01 18:58:07 -0500

    Update conf.py

--- shared/conf.py
+++ shared/conf.py
@@ -5,7 +5,7 @@ import os
 import sys
 import urllib
 
-import edx_theme
+#import edx_theme
 
 # on_rtd is whether we are on readthedocs.io, this line of code grabbed from docs.readthedocs.io
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
@@ -28,7 +28,7 @@ pygments_style = 'sphinx'
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'edx_theme',
+#    'edx_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
@@ -56,14 +56,14 @@ if on_rtd:
     html_context["github_base_account"] = 'edx'
     html_context["github_project"] = 'edx-documentation'
 
-html_theme = 'edx_theme'
-
-html_theme_path = [edx_theme.get_html_theme_path()]
-
-html_theme_options = {}
-html_theme_options['navigation_depth'] = 3
-
-html_favicon = os.path.join(edx_theme.get_html_theme_path(), 'edx_theme', 'static', 'css', 'favicon.ico')
+#html_theme = 'edx_theme'
+#
+#html_theme_path = [edx_theme.get_html_theme_path()]
+#
+#html_theme_options = {}
+#html_theme_options['navigation_depth'] = 3
+#
+#html_favicon = os.path.join(edx_theme.get_html_theme_path(), 'edx_theme', 'static', 'css', 'favicon.ico')
 
 # Help and Feedback links.  These are customized for the category and audience
 # of the book.  Add a line to the book's conf.py like this:
